4c7a2bd5481dd3466577779826a95a60d1f7e8b0
Petter Andersson <petter@pandersson.net>
2018-05-10 19:18:38 +0200

    Split graphs for better visualization

--- web/components/temp-chart/TempChart.vue
+++ web/components/temp-chart/TempChart.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="fermentation-stats-chart"></div>
+   <div>
+      <h4>Temperatur</h4>
+      <div id="temp-stats-chart"></div>
+      <h4>Gravity</h4>
+      <div id="gravity-stats-chart"></div>
+   </div>
 </template>
 
 <script>
@@ -8,35 +13,78 @@ import moment from "moment";
 import { GoogleCharts } from "google-charts";
 
 function formatDateTime(date) {
-  return moment(date).format("YYYY-MM-DD HH:mm:ss");
+  return moment(date).format("YYYY-MM-DD HH:mm  ");
 }
 
-function drawStatsChart(data) {
-  let dataTable = [["Tid", "Temperatur", "SG"]];
+function drawCharts(data) {
+  const tempData = data.map(s => {
+    return {
+      measured_at: s.measured_at,
+      temperature: s.temperature
+    };
+  });
+  const gravityData = data.map(s => {
+    return {
+      measured_at: s.measured_at,
+      gravity: s.gravity
+    };
+  });
+  drawTempChart(tempData);
+  drawGravityChart(gravityData);
+}
+
+function drawTempChart(data) {
+  let dataTable = [["Tid", "Temperatur"]];
   data.forEach(stat => {
     const time = formatDateTime(stat.measured_at);
     const temp = Utils.round10(stat.temperature, -1);
-    const gravity = parseFloat(stat.gravity);
-    dataTable.push([time, temp, gravity]);
+    dataTable.push([time, temp]);
   });
   const chartdata = GoogleCharts.api.visualization.arrayToDataTable(dataTable);
   const chart = new GoogleCharts.api.visualization.LineChart(
-    document.getElementById("fermentation-stats-chart")
+    document.getElementById("temp-stats-chart")
   );
   const options = {
     curveType: "function",
     legend: {
       position: "bottom"
+    },
+    vAxis: {
+      ticks: [5, 10, 15, 20, 25]
     }
   };
   chart.draw(chartdata, options);
 }
+
+function drawGravityChart(data) {
+  let dataTable = [["Tid", "SG"]];
+  data.forEach(stat => {
+    const time = formatDateTime(stat.measured_at);
+    const gravity = parseFloat(stat.gravity);
+    dataTable.push([time, gravity]);
+  });
+  const chartdata = GoogleCharts.api.visualization.arrayToDataTable(dataTable);
+  const chart = new GoogleCharts.api.visualization.LineChart(
+    document.getElementById("gravity-stats-chart")
+  );
+  const options = {
+    curveType: "function",
+    legend: {
+      position: "bottom"
+    },
+    vAxis: {
+      ticks: [1.0, 1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.1]
+    }
+  };
+  chart.draw(chartdata, options);
+}
+
 export default {
   props: ["temps"],
   created() {
     GoogleCharts.load(
       function() {
-        drawStatsChart(this.temps);
+        drawCharts(this.temps);
       }.bind(this)
     );
   },
@@ -45,7 +93,7 @@ export default {
       console.log(val);
       GoogleCharts.load(
         function() {
-          drawStatsChart(this.temps);
+          drawCharts(this.temps);
         }.bind(this)
       );
     }
